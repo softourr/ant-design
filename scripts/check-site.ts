b9c3b8d06b80ec4c9ca35d01c74a31a7a32b0018
owjs3901 <owjs3901@gmail.com>
2024-08-19 09:43:01 +0900

    Apply comment to unused var

--- scripts/check-site.ts
+++ scripts/check-site.ts
@@ -3,52 +3,52 @@
 import type http from 'http';
 import type https from 'https';
 import { join } from 'path';
-import { load } from 'cheerio';
-import { globSync } from 'glob';
+// import { load } from 'cheerio';
+// import { globSync } from 'glob';
 import { createServer } from 'http-server';
-import fetch from 'isomorphic-fetch';
-import uniq from 'lodash/uniq';
+// import fetch from 'isomorphic-fetch';
+// import uniq from 'lodash/uniq';
 
-const components = uniq(
-  globSync('components/!(overview)/*.md', { cwd: join(process.cwd()), dot: false }).map((path) =>
-    path.replace(/(\/index)?((\.zh-cn)|(\.en-us))?\.md$/i, ''),
-  ),
-).filter((component) => !component.includes('_util'));
+// const components = uniq(
+//   globSync('components/!(overview)/*.md', { cwd: join(process.cwd()), dot: false }).map((path) =>
+//     path.replace(/(\/index)?((\.zh-cn)|(\.en-us))?\.md$/i, ''),
+//   ),
+// ).filter((component) => !component.includes('_util'));
 
 describe('site test', () => {
   let server: http.Server | https.Server;
   const port = 3000;
-  const render = async (path: string) => {
-    const resp = await fetch(`http://127.0.0.1:${port}${path}`).then(async (res) => {
-      const html: string = await res.text();
-      const $ = load(html, {
-        xml: true,
-      });
-      return { status: res.status, $ };
-    });
-    return resp;
-  };
+  // const render = async (path: string) => {
+  //   const resp = await fetch(`http://127.0.0.1:${port}${path}`).then(async (res) => {
+  //     const html: string = await res.text();
+  //     const $ = load(html, {
+  //       xml: true,
+  //     });
+  //     return { status: res.status, $ };
+  //   });
+  //   return resp;
+  // };
+  //
+  // const handleComponentName = (name: string) => {
+  //   const [, componentName] = name.split('/');
+  //   return componentName.toLowerCase().replace('-cn', '').replace('-', '');
+  // };
 
-  const handleComponentName = (name: string) => {
-    const [, componentName] = name.split('/');
-    return componentName.toLowerCase().replace('-cn', '').replace('-', '');
-  };
-
-  const expectComponent = async (component: string) => {
-    const { status, $ } = await render(`/${component}/`);
-    expect(status).toBe(200);
-    expect($('h1').text().toLowerCase()).toMatch(handleComponentName(component));
-
-    /**
-     * 断言组件的 api table 数量是否符合预期。
-     * 在 #45066, #45017 中，因为 markdown 写法问题，导致 api table 无法渲染。
-     * 结合每个组件页的 table 数量变动，可以判断出是否存在问题。
-     * （table 数量相对比较稳定，如果 PR 有新增，则应该更新这里快照）
-     */
-    const tables = $('.markdown table');
-
-    expect(tables.length).toMatchSnapshot();
-  };
+  // const expectComponent = async (component: string) => {
+  //   const { status, $ } = await render(`/${component}/`);
+  //   expect(status).toBe(200);
+  //   expect($('h1').text().toLowerCase()).toMatch(handleComponentName(component));
+  //
+  //   /**
+  //    * 断言组件的 api table 数量是否符合预期。
+  //    * 在 #45066, #45017 中，因为 markdown 写法问题，导致 api table 无法渲染。
+  //    * 结合每个组件页的 table 数量变动，可以判断出是否存在问题。
+  //    * （table 数量相对比较稳定，如果 PR 有新增，则应该更新这里快照）
+  //    */
+  //   const tables = $('.markdown table');
+  //
+  //   expect(tables.length).toMatchSnapshot();
+  // };
 
   beforeAll(() => {
     server = createServer({ root: join(process.cwd(), '_site') });
