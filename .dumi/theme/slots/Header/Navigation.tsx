cfb00bea4e63a5fd166b16a7d1d61323737c0853
Kim Ji Yu <spbabo97@naver.com>
2024-10-06 23:27:48 +0900

    fix : 중국어에서만 뜨는 부분을 한국어에서 안뜨게 수정

--- .dumi/theme/slots/Header/Navigation.tsx
+++ .dumi/theme/slots/Header/Navigation.tsx
@@ -193,15 +193,15 @@ const HeaderNavigation: React.FC<NavigationProps> = (props) => {
       key: 'docs/resources',
     },
     isZhCN
-      ? {
+      ? null
+      : {
           key: 'mirror',
           label: (
             <a href="https://ant-design.antgroup.com" target="_blank" rel="noreferrer">
               国内镜像
             </a>
           ),
-        }
-      : null,
+        },
     ...(additional ?? []),
   ].filter(Boolean);
 
