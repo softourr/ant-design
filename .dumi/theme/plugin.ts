eea60bfae3e5ccd88dd9b47b0a4643feef125d47
Kim Ji Yu <spbabo97@naver.com>
2024-10-07 03:08:40 +0900

    fix : demo in md for ko-KR

--- .dumi/theme/plugin.ts
+++ .dumi/theme/plugin.ts
@@ -47,7 +47,8 @@ class AntdReactTechStack extends ReactTechStack {
 
     if (opts.type === 'external') {
       // try to find md file with the same name as the demo tsx file
-      const locale = opts.mdAbsPath.match(/index\.([a-z-]+)\.md$/i)?.[1];
+      // const locale = opts.mdAbsPath.match(/index\.([a-z-]+)\.md$/i)?.[1];
+
       const mdPath = opts.fileAbsPath!.replace(/\.\w+$/, '.md');
       const md = fs.existsSync(mdPath) ? fs.readFileSync(mdPath, 'utf-8') : '';
 
@@ -115,7 +116,7 @@ class AntdReactTechStack extends ReactTechStack {
         // Last block
         fillBlock(blockName, cacheList);
 
-        props.description = blocks[locale];
+        props.description = blocks['ko-KR'];
         props.style = blocks.style;
       }
     }
