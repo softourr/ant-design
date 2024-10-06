9e74da6683b83e1c00b4c7c401d933770c7d83b1
yeon-dong <rchijy@naver.com>
2024-10-06 18:27:57 +0900

    components button md 파일 번역

--- components/button/demo/basic.tsx
+++ components/button/demo/basic.tsx
@@ -5,7 +5,7 @@ const App: React.FC = () => (
   <Flex gap="small" wrap>
     <Button type="primary">Primary Button</Button>
     <Button>Default Button</Button>
-    <Button type="dashed">Dashed Button</Button>
+    <Button type="dashed">Dashed Button12</Button>
     <Button type="text">Text Button</Button>
     <Button type="link">Link Button</Button>
   </Flex>
